08b86ace495d3a910565b17bde13b92be985c2eb
tinkin <tinkin@tinkin.home>
2025-10-28 17:29:26 +0000

    Definir la commande pour archiver

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,17 +5,26 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use App\Jobs\VerifierBlocageJob;
+use Illuminate\Support\Facades\Artisan;
 
 class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        // 🔹 Tâche quotidienne à 02:00 pour archivage des comptes bloqués
+        // 🔹 Tâche quotidienne à 00:00 pour archivage des comptes bloqués
+        $schedule->call(function () {
+            Artisan::call('archive:sync');
+        })
+            ->dailyAt('00:00')
+            ->name('archivage-comptes-bloques')
+            ->withoutOverlapping();
+
+        // 🔹 Tâche quotidienne à 02:00 pour vérification des blocages
         $schedule->call(function () {
             VerifierBlocageJob::dispatch()->onQueue('archivage');
         })
-            ->dailyAt('15:30')
-            ->name('archivage-comptes-bloques')
+            ->dailyAt('02:00')
+            ->name('verification-blocages')
             ->withoutOverlapping();
     }
 
